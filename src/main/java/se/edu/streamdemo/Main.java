e80b7146afdc87bbbce80b4c53b95fa742b06a8b
Li_Kunjun <StargaerX0>
2024-02-25 11:51:50 +0800

    lambda

--- src/main/java/se/edu/streamdemo/Main.java
+++ src/main/java/se/edu/streamdemo/Main.java
@@ -6,20 +6,25 @@ import se.edu.streamdemo.task.Task;
 
 import java.util.ArrayList;
 
+import static java.util.stream.Collectors.toList;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Task (stream) manager");
         DataManager dataManager = new DataManager("./data/data.txt");
         ArrayList<Task> tasksData = dataManager.loadData();
 
-        System.out.println("Printing all data ...");
-        printAllData(tasksData);
+//        System.out.println("Printing all data ...");
+//        printAllData(tasksData);
 
         System.out.println("Printing deadlines ...");
         printDeadlines(tasksData);
-
-        System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
-
+        System.out.println("Printing deadlines ...");
+        printDeadlinesUsingStream(tasksData);
+//
+//        System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
+        ArrayList<Task> filteredList = filterByString(tasksData, "11");
+        printAllData(filteredList);
     }
 
     private static int countDeadlines(ArrayList<Task> tasksData) {
@@ -44,6 +49,21 @@ public class Main {
                 System.out.println(t);
             }
         }
+    }
+
+    public static void printDeadlinesUsingStream(ArrayList<Task> tasks) {
+        tasks.stream()
+            .filter((t) -> t instanceof Deadline)
+            .sorted((t1, t2) -> t1.getDescription().compareToIgnoreCase((t2.getDescription())))
+            .forEach(System.out::println);
+    }
+
+    public static ArrayList<Task> filterByString(ArrayList<Task> tasks, String filterString) {
+        ArrayList<Task> filteredList = (ArrayList<Task>) tasks.stream()
+            .filter((t) -> t.getDescription().contains(filterString))
+            .collect(toList());
+
+        return filteredList;
     }
 
 }
